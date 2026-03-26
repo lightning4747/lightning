d5e45972f9a078793b715baea8f02d572b0a65d1
lightning4747 <vignesh112847@gmail.com>
2026-03-26 18:54:12 +0530

    chore: move the navbar to the top postition

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -29,7 +29,7 @@ export const Navbar = () => {
       animate={{ y: 0 }}
       transition={{ duration: 0.8, ease: [0.22, 1, 0.36, 1] }}
       className={`
-        fixed top-0 left-0 right-0 z-50 transition-all duration-500
+        fixed top-0 left-0 right-0 z-[1000] transition-all duration-500
         ${isScrolled ? 'py-4 glass-effect' : 'py-8 bg-transparent'}
       `}
     >
